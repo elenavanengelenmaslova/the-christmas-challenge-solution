9ca27982a394c013fb929c4c267cf7639e78bf68
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-12-22 14:00:55 +0100

    add velocity

--- infrastructure/src/main/kotlin/nl/vintik/workshop/aws/infra/InfrastructureChristmasStack.kt
+++ infrastructure/src/main/kotlin/nl/vintik/workshop/aws/infra/InfrastructureChristmasStack.kt
@@ -147,7 +147,21 @@ class InfrastructureChristmasStack(scope: Construct, id: String, props: StackPro
             BaseResolverProps.builder()
                 .typeName("Query")
                 .fieldName("getReindeerByName")
-                .requestMappingTemplate(MappingTemplate.dynamoDbQuery(KeyCondition.eq("name", "name"), "reindeer-name-index"))
+
+                .requestMappingTemplate(
+                    MappingTemplate.fromString(
+                        "{\n" +
+                                "    \"version\": \"2017-02-28\",\n" +
+                                "    \"operation\": “Query”,\n" +
+                                "    “query”: {\n" +
+                                "        “expression” : “name = :name” ,\n" +
+                                "        “expressionValues” : {\n" +
+                                "                  “:name”:  \$util.dynamodb.toDynamoDBJson(\$ctx.args.name)\n" +
+                                "         }\n" +
+                                "    }\n" +
+                                "}"
+                    )
+                )
                 .responseMappingTemplate(MappingTemplate.dynamoDbResultItem())
                 .build()
         )
